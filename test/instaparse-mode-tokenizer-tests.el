27bff2011006c51610c338ba2580ac2f119303b8
fbauer <fbauer.devel@gmail.com>
2014-08-22 13:06:23 +0200

    Tokenize a smallish sample buffer

--- test/instaparse-mode-tokenizer-tests.el
+++ test/instaparse-mode-tokenizer-tests.el
@@ -1,9 +1,13 @@
-(instaparse-deftest tokenizer-test ()
-  (instaparse-ert-with-test-buffer (:name "tokenizer-test")
+(instaparse-deftest forward-tokenizer-test ()
+  (instaparse-ert-with-test-buffer (:name "forward-tokenizer-test")
       "foo-bar := baz
 bar-bar := baz"
-      (progn
-        (goto-char (point-min))
-        (let ((tok (instaparse-smie-forward-token)))
-          (should (string= tok  "foo-bar"))))))
+    (progn
+      (goto-char (point-min))
+      (dolist (expected-token '("foo-bar" "=" "baz"
+                                "bar-bar" "=" "baz"))
+        (let ((token (instaparse-smie-forward-token)))
+          (should (string= token expected-token))))
+      (should (= (point) (point-max))))))
+
 
